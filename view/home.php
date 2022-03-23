b027b71ca65fb0d25a71790accd76c01c50313c8
SAMI RICHARDS <samirichards100@gmail.com>
2022-03-23 19:12:03 +0000

    First commit

---
+++ view/home.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Home</title>
+        <link rel="stylesheet" href="../dist/app.css"/>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <script src="../dist/app.js"></script>
+    </head>
+    <body>
+        <p id="test1">Welcome</p>
+    </body>
+</html>
